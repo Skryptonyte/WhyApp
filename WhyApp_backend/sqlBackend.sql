0aa4b6394481f1e3f0d788bbefc4786bd3da0aad
Rayhan Faizel <rayhan.faizel@gmail.com>
2022-05-14 00:52:38 +0530

    Add table creation to sql script

--- WhyApp_backend/sqlBackend.sql
+++ WhyApp_backend/sqlBackend.sql
@@ -1,3 +1,8 @@
+
+create table chatuser (user_id int, username varchar(30), password varchar(30), joindate date, primary key(user_id));
+create table rooms (room_id int, room_name varchar(20), room_desc varchar(100), primary key(room_id));
+create table posts (post_id int, user_id int, room_id int, content varchar(256), createdate timestamp, primary key(post_id), foreign key(user_id) references chatuser, foreign key(room_id) references rooms)
+
 create sequence userIDGenerator INCREMENT BY 1 START WITH 1;
 
 create or replace trigger populateUserID
